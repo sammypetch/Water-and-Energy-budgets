1b0111262449a25a5ab90f7b93234f5677f4509b
sammypetch <94913635+sammypetch@users.noreply.github.com>
2022-10-14 13:39:39 +0100

    Update Optimisation.py

--- Optimisation.py
+++ Optimisation.py
@@ -52,6 +52,7 @@ def optimisation(month):
 b = # USER DEFINE BASIN NUMBER
 
 Pobs,Qobs, LEobs, dSobs, DSRobs, DLRobs, USWobs ,ULWobs, SHobs , NETobs = basin_fluxes(b) # basin observed fluxes
+Perror,Qerror, LEerror, DSRerror, DLRerror, USWerror, ULWerror , SHerror = basin_unertainty(b)
 GRACE_stom =  stom_storage(b) # start of month GRACE water storage  data 
 FISeD =  calculate_FISeD(b) # Flux inferred energy storage 
 
